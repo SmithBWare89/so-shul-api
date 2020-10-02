6c02c17c49dddd81025cef123b83c64c420da57c
Stephon Smith <smithwrestling89@gmail.com>
2020-10-02 09:13:38 -0400

    initial commit and model creation

---
+++ models/Thought.js
@@ -0,0 +1,69 @@
+const moment = require('moment');
+const {Schema, model, Types} = require('mongoose');
+
+const reactionSchema = (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            trim: true
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const thoughtSchema = (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount', function() {
+    return this.reactions.length;
+})
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/so-shul', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+// Error handling for database
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+})
